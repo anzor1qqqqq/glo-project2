e07a545650f4c0d05615a703c5b9cacb6e048e0d
Anzor <anzor.shadzhe@gmail.com>
2023-02-25 19:18:41 +0300

    modified:   dist/index.html 	modified:   dist/script/main.js 	modified:   src/index.js 	modified:   src/modules/modal.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@
 import time from './modules/time';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import reg from './modules/regular';
 
 time('1 march 2023');
 menu();
 modal();
+reg();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const openModal = () => {
     const popupBtn = document.querySelectorAll('.popup-btn');

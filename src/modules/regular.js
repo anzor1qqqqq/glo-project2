eff8a111eaaef55c8984d9e2c3029f02c52c5e21
Anzor <anzor.shadzhe@gmail.com>
2023-02-25 22:24:11 +0300

    Усложненка

--- src/modules/regular.js
+++ src/modules/regular.js
@@ -9,24 +9,25 @@ const checkRegExp = () => {
     calcItem.forEach(item => {
         item.addEventListener('blur', elem => {
             let protectCalc = elem.target.value.toLowerCase().match(/[a0-9z-а0-9я]/g).join('');
+            console.log('protectCalc: ', protectCalc);
             protectCalc = protectCalc.replace(/[a-z-а-я]/g, '');
             elem.target.value = protectCalc;
         });
     });
 
     messInput.addEventListener('blur', elem => {
-        let protect;
+        let protectMess;
 
         if (elem.target.value.match(/[\а-яА-Я\-?\s]+/g)) {
-            protect = elem.target.value.toLowerCase().match(/[\а-яА-Я\-?\s]+/g).join('').trim();
+            protectMess = elem.target.value.toLowerCase().match(/[\а-яА-Я\-?\s]+/g).join('').trim();
 
-            protect = protect.replace(/\-+$/g, '');
-            protect = protect.replace(/^\-+/g, '');
-            protect = protect.replace(/\s{2,}/g, ' ');
-            protect = protect.replace(/\-{2,}/g, '-');
+            protectMess = protectMess.replace(/\-+$/g, '');
+            protectMess = protectMess.replace(/^\-+/g, '');
+            protectMess = protectMess.replace(/\s{2,}/g, ' ');
+            protectMess = protectMess.replace(/\-{2,}/g, '-');
 
-            let splited = protect[0].toUpperCase();
-            let splited2 = [...protect].splice(1, );
+            let splited = protectMess[0].toUpperCase();
+            let splited2 = [...protectMess].splice(1,);
 
             splited2.unshift(splited);
             splited2 = splited2.join(''); 
@@ -39,15 +40,36 @@ const checkRegExp = () => {
 
     emailInput.forEach(item => {
         item.addEventListener('blur', elem => {
-            console.log(elem.target.value.toLowerCase().match(/[^\a-zA-Z?\.?\-?\_?\!?\~?\*?\'?\@]+$/g, ''));
+            let protectEmail;
+
+            if (elem.target.value.toLowerCase().match(/[\a-zA-Z0-9?\.?\-?\_?\!?\~?\*?\'?\@]+/g)) {
+                protectEmail = elem.target.value.toLowerCase().match(/[\a-zA-Z0-9?\.?\-?\_?\!?\~?\*?\'?\@]+/g, '').join('').trim();
+
+                elem.target.value = protectEmail;
+            } else {
+                elem.target.value = '';
+            };
         });
     });
-/*
+
     telInput.forEach(item => {
-        item.addEventListener('input', elem => {
-            elem.target.value = elem.target.value.replace(/[^\d\-\()]+/g, '');
+        item.addEventListener('blur', elem => {
+            let protectNumTel;
+
+            if (elem.target.value.match(/[\d\-\()]+/g)) {
+                protectNumTel = elem.target.value.match(/[\d\-\()]+/g).join('').trim();
+                
+                protectNumTel = protectNumTel.replace(/\-+$/g, '');
+                protectNumTel = protectNumTel.replace(/^\-+/g, '');
+                protectNumTel = protectNumTel.replace(/\s{2,}/g, ' ');
+                protectNumTel = protectNumTel.replace(/\-{2,}/g, '-');
+
+                elem.target.value = protectNumTel
+            } else {
+                elem.target.value = '';
+            }
         });
-    }); */
+    }); 
 };
 
 export default checkRegExp;

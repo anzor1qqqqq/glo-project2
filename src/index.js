4c9c7ac560ee2244bce221ab5057723a7dfdecbd
Anzor <anzor.shadzhe@gmail.com>
2023-03-03 22:45:16 +0300

    modified:   dist/index.html 	modified:   dist/script/main.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import reg from './modules/regular';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 time('15 march 2023');
 menu();
 modal();
 reg();
 tabs();
+slider();
